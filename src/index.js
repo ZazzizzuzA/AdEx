8b7ef0fc1cb3c125761ec331e813b574e035f360
Mykhailo Pylypenko <musicvyp@gmail.com>
2018-04-23 02:24:07 +0300

    addEventListener, throttle, googletags

--- src/index.js
+++ src/index.js
@@ -1,47 +1,111 @@
 import "./styles/styles";
 import "./controller/ctrl.js";
 
-let commer1 = document.getElementById('ad1'),
-    commer2 = document.getElementById('ad2'),
+let allCommer = Array.from(document.getElementsByClassName('ad')),
+    commer1 = allCommer[0],
+    commer2 = allCommer[1],
     body = document.getElementsByTagName('body')[0],
     position,
-    allCommer = Array.from(document.getElementsByClassName('ad'));
+    existCommers = true,
+    content = document.getElementsByClassName("block")[0],
+    slot1 = googletag.defineSlot('/112081842/Front-End-TA_240x400', [240, 400], "ad1"),
+    slot2 = googletag.defineSlot('/112081842/Front-End-TA2_240x400', [240, 400], 'ad2');
+    console.log(slot1);
+    console.log(slot2);
 
-commer1.setAttribute( "style", "position: absolute; top: calc(50vh - 200px + 20vh); left: 0; display: block");
-commer2.setAttribute( "style", "position: absolute; top: calc(50vh - 200px + 20vh); right: 0; display: block");
+commer1.setAttribute( "style", "position: absolute; top: calc(50vh - 250px + 20vh); left: 0;");
+commer2.setAttribute( "style", "position: absolute; top: calc(50vh - 250px + 20vh); right: 0;");
 
-window.onscroll = (event) => {
+/*      Функция задержки      */
 
+function throttle(func, ms) {
+
+    var isThrottled = false,
+      savedArgs,
+      savedThis;
+  
+    function wrapper() {
+  
+      if (isThrottled) {
+        savedArgs = arguments;
+        savedThis = this;
+        return;
+      }
+  
+      func.apply(this, arguments); 
+  
+      isThrottled = true;
+  
+      setTimeout(function() {
+        isThrottled = false;
+        if (savedArgs) {
+          wrapper.apply(savedThis, savedArgs);
+          savedArgs = savedThis = null;
+        }
+      }, ms);
+    }
+    return wrapper;
+  };
+
+  /*     При изменении размера window реклама исчезает/появляется соответственно      */
+
+console.log(googletag.openConsole());
+
+  window.addEventListener( "resize", throttle( function() {
+ 
+    if (body.offsetWidth <= 768 && existCommers == true) {
+
+      /*     googletag.destroySlots() не удаляет, хотя в консоли (googletag.openConsole()) написано, что рекламное место удалено  */
+
+        allCommer.forEach( (item) => {
+          item.style.display = "none";
+        });
+        googletag.destroySlots();
+
+        content.style.width = "100%";
+        existCommers = false;
+
+    } else if (body.offsetWidth > 768 && existCommers == false) {
+        existCommers = true;
+
+        allCommer.forEach( (item) => {
+          item.style.display = "block";
+        });
+
+        content.style.width = "calc(100vw - 500px)";
+    }
+  }, 100) );
+
+  /*       Блоки принимают позицию fixed при скроллинге        */
+
+  window.addEventListener( 'scroll', throttle( function() {
     position = window.pageYOffset;
 
-    if (window.outerWidth > 768 && window.innerWidth > 768) {
+    if (existCommers == true) {
         if (position >= 140) {
-            commer1.setAttribute( "style", "position: fixed; top: calc(50vh - 200px); left: 0; display: block");
-            commer2.setAttribute( "style", "position: fixed; top: calc(50vh - 200px); right: 0; display: block");
+            commer1.setAttribute( "style", "position: fixed; top: calc(50vh - 250px); left: 0;");
+            commer2.setAttribute( "style", "position: fixed; top: calc(50vh - 250px); right: 0;");
         }
         if (position < 140) {
-            commer1.setAttribute( "style", "position: absolute; top: calc(50vh - 200px + 20vh); left: 0; display: block");
-            commer2.setAttribute( "style", "position: absolute; top: calc(50vh - 200px + 20vh); right: 0; display: block");
+            commer1.setAttribute( "style", "position: absolute; top: calc(50vh - 250px + 20vh); left: 0;");
+            commer2.setAttribute( "style", "position: absolute; top: calc(50vh - 250px + 20vh); right: 0;");
         }
+  console.log(position)
     }
-};
+}, 100), {
+  passive: true
+} );
+
+/*      При запуске на маленьких экранах контент на всю ширину. Условие для скрипта рекламы записано в ДОМе.      */
+
 if (window.outerWidth <= 768 || window.innerWidth <= 768) {
-    allCommer.forEach( (item) => {
-        item.style.display = "none"
-    })
-    console.log(window.outerWidth);
+
+    content.style.width = "100%";
+    existCommers = false;
     
 };
-window.onresize = () => {
-    if (window.innerWidth <= 768 || window.outerWidth <= 768) {
-        allCommer.forEach( (item) => {
-            item.style.display = "none"
-        })
-    } else if (window.innerWidth > 768 || window.outerWidth > 768) {
-        commer1.setAttribute( "style", "position: absolute; top: calc(50vh - 200px + 20vh); left: 0; display: block");
-        commer2.setAttribute( "style", "position: absolute; top: calc(50vh - 200px + 20vh); right: 0; display: block");
-    }
-}
-console.log(window.outerWidth);
+
+
+
 
 
